ec54db1e1f01118e35e90d3f0fbde2126eb19ec9
addhyan94 <addhyant85@gmail.com>
2026-05-30 17:15:27 +0530

    Data Type..

---
+++ 01-Basic-JS/DataType.js
@@ -0,0 +1,26 @@
+"use strict"; //ye ek keyword hai jo JS ke code ko ek new version me treat karta hai ..
+
+// alert("Hello I'm Alert msg...");
+// ye alert serf browser per he chalta hai i meen DOM use karta hai nodejs ni 
+
+// Data types
+name = "hello" // ye ek string data type hai 
+age = 20 // ye ek number data type hai 
+islogin = false // ye ek boolean data type hai jo true / false me hota hai serf ...
+ 
+
+// number => 2 to power 53
+// bigint => 52 se badi ( but jada ter normal numbers ko he use karte hai )
+// string => ""/ '' ( iske aander bali he string hoti hai )
+// boolean => true/false 
+// null => standalone value 
+// undefined => jisko aabhi define ni kiya ho na null na koi number na kuch 
+// symbol => unique
+
+
+// object => 
+//  typeof ka use kisi bhi chezz ka type dekhne ek liye use hota hai jese ->
+
+console.log(typeof 20); // number
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
