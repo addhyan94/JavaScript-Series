db0a3651a2adb360710a422faac1c9c1da8de469
addhyan94 <addhyant85@gmail.com>
2026-07-30 10:38:22 +0530

    Operations.js File

---
+++ 01-Basic-JS/operations.js
@@ -0,0 +1,22 @@
+let value = 4;
+let negvalue = -value;
+console.log(value);
+console.log(negvalue);
+/* yaha aapan ko result mila 4 and -4 beacuse uper aapan ne negvalue me (-) laga diya tha to valye (-) me he mili  */
+
+// yr notms; opernations hai jo 11-12 me bhi the 😅 
+console.log(2+2); // add
+console.log(2-2); //sub
+console.log(2*2); // multiply
+console.log(2/2); // devide
+console.log(2**3); // power
+console.log(2%3); // remender 
+
+// 2 string ko sath me add kr sakte hai
+let str1 ="hello"
+let str2 = " world"
+
+let str3 = str1 + str2 ;
+console.log(str3);
+
+/* result( hello world ) aaya ... */
