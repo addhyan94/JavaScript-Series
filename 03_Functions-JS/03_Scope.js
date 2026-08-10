60c63453b599d9adecdc7bd766bd51e30576850f
addhyan94 <addhyant85@gmail.com>
2026-08-11 01:14:03 +0530

    Scops ... {}

---
+++ 03_Functions-JS/03_Scope.js
@@ -0,0 +1,20 @@
+// Scope 
+/*
+let a =10 
+const b = 20 
+var c = 30
+console.log(a, b, c);
+*/
+// {} this is a Scope ... ye her he cheez me aata hai . but Object me jo {} ye laga hota hota hai bo Scope ni hota hai bo Object deceleation hota hai 
+/*
+if (true) {
+    let a = 10
+    const b = 20
+    var c= 30
+}
+// console.log(a); // a if ke aander decelear hua hai to bo if ke bahar ni aayega 
+// console.log(b);// same b bhi if ke aander decelear hua hai to bo if ke bahar ni aayega 
+console.log(c); // But c if ke aander decelear hua tha butt butt butt bo bahar aa gaya .. or ese ni hocna chaiye .. kuki scope ke aander jo bhi kuch decelear hua ho usko global ni rahena chaiye bo local he rahena chaiye isliye VAR ka use ni karna chaiye 
+
+*/
+
