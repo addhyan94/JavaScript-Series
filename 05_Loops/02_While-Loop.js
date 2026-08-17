89afa5454baeee6768965abbf52cb02919b8b73f
addhyan94 <addhyant85@gmail.com>
2026-08-17 18:00:20 +0530

    While Loop

---
+++ 05_Loops/02_While-Loop.js
@@ -0,0 +1,24 @@
+/* Start While Loop  */
+// syntax ->.
+// let index = 0 // start loop point
+// while (index <=10) { // only check condition
+    // code................
+// index++ // increase number 
+// }
+
+let i = 0
+while (i < 10) {
+    console.log(`value is : ${i}`);
+    // i++ // isme serf +1 ho rahah tha 
+   i= i+2 // isme +2 kr diya to 0+2 ese aayega fer 
+}
+
+
+/*Output ->
+value is : 0
+value is : 2
+value is : 4
+value is : 6
+value is : 8
+
+*/
