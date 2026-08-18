b09f17d7098f7aea68934947aa47f33b29aa1b5b
addhyan94 <addhyant85@gmail.com>
2026-08-18 17:45:04 +0530

    Do While Loop

---
+++ 05_Loops/03_Do-While-Loop.js
@@ -0,0 +1,16 @@
+/*Now Start Do While Loop */
+
+// syntax -> 
+// do {
+    
+// } while (condition);
+
+/* yaha kya hua ki phale kaam kiya fer condition check ki  */
+let i = 10
+do {
+    console.log('value is :' + i);
+} while (i<10);
+// yaha i 10 se chota ni tha but fer bhi ek br kaam kr diya 
+ /* Best example ever --> Phale istmaal karo fer bisbaas karo (do-while) 🤣🤣 */
+
+ 
